cde2334a42eb73289988b665926ce5706eeac55f
evethebuilder <eveychoi@gmail.com>
2024-06-29 19:46:25 +0800

    Update streamlit_app.py
    indentations

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,18 +42,18 @@ if ingredients_list:
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
             fv_df = st.dataframe (data=fruityvice_response.json(), use_container_width=True)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
-    #st.write(my_insert_stmt)
-    #st.stop()
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                    values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     
-    time_to_insert = st.button ('Submit Order')
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+        #st.write(my_insert_stmt)
+        #st.stop()
         
-        st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
+        time_to_insert = st.button ('Submit Order')
+    
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            
+            st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
         
 import requests
 
