d33e40743c9030b1b87f8382caff9e9328eb4d24
evethebuilder <eveychoi@gmail.com>
2024-06-29 19:58:28 +0800

    Update streamlit_app.py
    moved the import statement to the top around line 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,22 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom Smoothie!"""
-    #"""Replace this example with your own code!
-    #**And if you're new to Streamlit,** check
-    #out our easy-to-follow guides at
-    #[docs.streamlit.io](https://docs.streamlit.io).
-    #"""
-)
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
-
-
-#option = st.selectbox(
-#    "What is your favorite fruit?",
-#    ("Banana", "Strawberries", "Peaches"))
-
-#st.write("Your favorite fruit is:", option)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    ,my_dataframe    
-    ,max_selections=6
-    )
+        'Choose up to 5 ingredients:'
+        ,my_dataframe
+        ,max_selections=6
+        )
 
 if ingredients_list:
         ingredients_string = ''
@@ -49,7 +33,5 @@ if ingredients_list:
     
         if time_to_insert:
                 session.sql(my_insert_stmt).collect()
-                st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")   
-
-import requests
+                st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
 
